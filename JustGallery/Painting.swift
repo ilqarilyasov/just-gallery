f2a6a616cd12a83d1fd9c5ff3bc85ef140385a7c
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-01-15 10:23:52 -0600

    Add model object

---
+++ JustGallery/Painting.swift
@@ -0,0 +1,21 @@
+//
+//  Painting.swift
+//  JustGallery
+//
+//  Created by Ilgar Ilyasov on 1/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+struct Painting {
+    let image: UIImage
+    var isLiked: Bool
+    
+    init(image: UIImage, isLiked: Bool = false) {
+        self.image = image
+        self.isLiked = isLiked
+    }
+}
+
+
